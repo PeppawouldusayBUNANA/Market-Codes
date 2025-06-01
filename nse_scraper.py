a355c42c3f7958f7a1eeb7aa30293e6722e8856a
PeppawouldusayBUNANA <ansh_tiwari2808@outlook.com>
2025-06-01 10:48:46 +0530

    The main file that contains all functions used in main.py

--- nse_scraper.py
+++ nse_scraper.py
@@ -1,8 +1,8 @@
 # nse_scraper.py
-from prettytable import PrettyTable                 # type: ignore
-from selenium import webdriver                      # type: ignore
-from selenium.webdriver.edge.service import Service # type: ignore
-from selenium.webdriver.edge.options import Options # type: ignore
+from prettytable import PrettyTable
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service 
+from selenium.webdriver.edge.options import Options 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
