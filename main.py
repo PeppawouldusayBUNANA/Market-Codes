7c3d22552d94ee12c0690e58bf852d259dd694e8
PeppawouldusayBUNANA <ansh_tiwari2808@outlook.com>
2025-06-01 10:51:16 +0530

    Runs the functions from **nse_scraper.py**

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from nse_scraper import *  #type: ignore
 
 edgeDriver = "C:/Users/ADMIN/Downloads/edgedriver_win64/msedgedriver.exe"
 
-headers, cookies = get_headers_and_cookies(edgeDriver)     #type: ignore
+headers, cookies = get_headers_and_cookies(edgeDriver)
 
-underlyingValue = get_underlying_value(headers, cookies)   #type: ignore
+underlyingValue = get_underlying_value(headers, cookies) 
 
-optionData = get_option_chain(headers, cookies)            #type: ignore
+optionData = get_option_chain(headers, cookies)
 strike_data = optionData['records']['data']
 
 
-print_option_table(strike_data, underlyingValue)           #type: ignore
+print_option_table(strike_data, underlyingValue)
 
